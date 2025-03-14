251ffac7af5c5d50f1b57ba39f7e1389e2fcd5f8
unknown <lovakumar.g@gmail.com>
2025-03-14 16:28:54 +0530

    initial commit

---
+++ src/main/java/com/glktech/gitpractice/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.glktech.gitpractice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class EmployeeController {
+
+      @GetMapping("/wishme")
+      public String generateWishMsg(){
+        return "Hello Good Afternoon !";
+      }
+}
